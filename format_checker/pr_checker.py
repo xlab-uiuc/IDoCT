fa5ba3689ea31d9b5c078684525c0a6dd6b3cc52
Alex-Lian <lian7@illinois.edu>
2023-09-10 14:54:38 -0500

    fix

--- format_checker/pr_checker.py
+++ format_checker/pr_checker.py
@@ -18,14 +18,14 @@ meta_data = {
         "TEST_NAME",
         "VALUE",
         "TYPE(GOOD|BAD)",
-        "EXPECTATION(PASS|FAIL)",
+        "RESULT(PASS|FAIL)",
         "COVERAGE_CHANGE(YES|NO)",
     ],
     "Type": [
         "GOOD",
         "BAD",
     ],
-    "Expectation": [
+    "Result": [
         "PASS",
         "FAIL",
     ],
@@ -43,11 +43,11 @@ def check_type(filename, row, i, log):
         log_std_error(filename, log, i, row, "TYPE(GOOD|BAD)")
 
 
-def check_expectation(filename, row, i, log):
+def check_result(filename, row, i, log):
     """Check validity of Expectation."""
 
-    if not row["EXPECTATION(PASS|FAIL)"] in meta_data["Expectation"]:
-        log_std_error(filename, log, i, row, "EXPECTATION(PASS|FAIL)")
+    if not row["RESULT(PASS|FAIL)"] in meta_data["Result"]:
+        log_std_error(filename, log, i, row, "RESULT(PASS|FAIL)")
 
 
 def check_coverage(filename, row, i, log):
@@ -63,7 +63,7 @@ def run_checks_pr(log, filename):
         check_row_length,
         check_common_rules,
         check_type,
-        check_expectation,
+        check_result,
         check_coverage,
     ]
     run_checks(filename, meta_data, log, checks)
